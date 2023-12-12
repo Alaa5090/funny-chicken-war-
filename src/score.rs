856199bd070cb533a02f86201ebda90795713185
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 14:40:42 +0200

    create Score struct

---
+++ src/score.rs
@@ -0,0 +1,16 @@
+use crate::frame::{Drawable, Frame};
+
+#[derive(Default)]
+pub struct Score {
+    count: u16,
+}
+
+impl Score {
+    pub fn new() -> Self {
+        Self { count: 0 }
+    }
+
+
+
+
+}
