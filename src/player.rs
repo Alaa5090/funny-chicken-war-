7b5eadf961a36ed6ab77c5f6dcc5c879d40654fd
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 11:46:14 +0200

    impl (shoot ,update,detect_hits)functions for Player

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,7 @@
 use crate::{
     frame::{Drawable, Frame},
-    //invaders::Invaders,
+    invader_player::InvaderPlayer,egg_shot::EggShot,
+    invaders::Invaders,
     shot::Shot,
     {NUM_COLS, NUM_ROWS},
 };
@@ -9,10 +10,9 @@ use std::time::Duration;
 pub struct Player {
     pub x: usize,
     pub y: usize,
-    
+
     shots: Vec<Shot>,
-    pub killed:bool,
-    
+    pub killed: bool,
 }
 impl Player {
     pub fn new() -> Self {
@@ -20,34 +20,59 @@ impl Player {
             x: NUM_COLS / 2,
             y: NUM_ROWS - 3,
             shots: Vec::new(),
-            killed:false
+            killed: false,
         }
     }
 
-pub fn move_left(&mut self) {
-    if self.x >=1 {
-        self.x -= 1;
+    pub fn move_left(&mut self) {
+        if self.x >= 1 {
+            self.x -= 1;
+        }
+    }
+    pub fn move_right(&mut self) {
+        if self.x < NUM_COLS - 1 {
+            self.x += 1;
+        }
+    }
+    pub fn kill_player_at(&mut self, x: usize, y: usize) -> bool {
+        if self.x == x && self.y == y
+        //position(|player| (player.x == x) && (player.y == y))
+        {
+            self.killed = true;
+            self.x = NUM_COLS / 2;
+            self.y = NUM_ROWS - 1;
+            true
+        } else {
+            false
+        }
     }
 }
-pub fn move_right(&mut self) {
-    if self.x < NUM_COLS - 1 {
-        self.x += 1;
+impl InvaderPlayer for Player {
+    fn shoot(&mut self) -> bool {
+        if self.shots.len() < 2 {
+            self.shots.push(Shot::new(self.x, self.y - 1));
+            true
+        } else {
+            false
+        }
     }
-}
-pub fn kill_player_at(&mut self, x: usize, y: usize) -> bool {
-    if  self.x==x && self.y==y//position(|player| (player.x == x) && (player.y == y))
-    {
-        self.killed=true;
-        self.x=NUM_COLS / 2;
-        self.y=NUM_ROWS - 1;
-        true
-    } 
-    else {
-        false
+     fn update(&mut self, delta: Duration) {
+        for shot in self.shots.iter_mut() {
+            shot.update(delta);
+        }
+        self.shots.retain(|shot| !shot.dead());
     }
-}
-
-
-
-
+     fn detect_hits(&mut self, invaders: &mut Invaders) -> u16 {
+        let mut hit_something = 0u16;
+        for shot in self.shots.iter_mut() {
+            if !shot.exploding {
+                let hit_count = invaders.kill_invader_at(shot.x, shot.y);
+                if hit_count > 0 {
+                    hit_something += hit_count;
+                    shot.explode();
+                }
+            }
+        }
+        hit_something
+    }
 }
