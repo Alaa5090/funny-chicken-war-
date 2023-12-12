25cea4462d5e557729d4c82a8a026e7769b2bf12
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 11:31:14 +0200

    impl (shoot,update,detect_hits)functions for Invader

--- src/invaders.rs
+++ src/invaders.rs
@@ -1,7 +1,7 @@
 use crate::{
     frame::{Drawable, Frame},
     {NUM_COLS, NUM_ROWS},
-    egg::Egg, player::Player,
+    egg::Egg, player::Player, invader_player::InvaderPlayer,egg_shot::EggShot,
 };
 use rusty_time::timer::Timer;
 use std::{cmp::max, time::Duration};
@@ -23,6 +23,27 @@ impl  Invader{
             eggs: Vec::new(),
         }
     } 
-}
+} 
+impl InvaderPlayer for Invader {
+    fn shoot(&mut self) -> bool {
+        if self.eggs.len() < 2 {
+            self.eggs.push(Egg::new(self.x, self.y - 1));
+            true
+        } else {
+            false
+        }
+    }
+    fn update(&mut self, delta: Duration) {
+        for egg in self.eggs.iter_mut() {
+            egg.update(delta);
+        }
+        self.eggs.retain(|egg| !egg.dead());
+    }
+
+    fn detect_hits(&mut self, player: &mut Player) {
+        todo!()
+    }
+     
+}  
 
 
