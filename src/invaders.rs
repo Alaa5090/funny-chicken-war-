d4e458ec8fcf7e9b69ca84fbafc2547d4728fced
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 12:01:36 +0200

    Add update function for Invaders

--- src/invaders.rs
+++ src/invaders.rs
@@ -85,4 +85,41 @@ impl Invaders {
             direction: 1,
         }
     }
+
+    pub fn update(&mut self, delta: Duration) -> bool {
+        self.move_timer.update(delta);
+        if self.move_timer.ready {
+            self.move_timer.reset();
+            let mut downwards = false;
+            if self.direction == -1 {
+                let min_x = self.army.iter().map(|invader| invader.x).min().unwrap_or(0);
+                if min_x == 0 {
+                    self.direction = 1;
+                    downwards = true;
+                }
+            } else {
+                let max_x = self.army.iter().map(|invader| invader.x).max().unwrap_or(0);
+                if max_x == NUM_COLS - 1 {
+                    self.direction = -1;
+                    downwards = true;
+                }
+            }
+
+            if downwards {
+                let new_duration = max(self.move_timer.duration.as_millis() - 250, 250);
+                self.move_timer = Timer::from_millis(new_duration as u64);
+                for invader in self.army.iter_mut() {
+                    invader.y += 1;
+                }
+            }
+            // } else {
+            //     for invader in self.army.iter_mut() {
+            //         invader.x = ((invader.x as i32) + self.direction) as usize;
+
+            //     }
+            //}
+            return true;
+        }
+        false
+    }
 }
