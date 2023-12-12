770cf707934394fc48586cb78a88d6d9a46f9f5b
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 14:46:44 +0200

    create Level Struct

---
+++ src/level.rs
@@ -0,0 +1,18 @@
+use crate::frame::{Drawable, Frame};
+
+const MAX_LEVEL: u8 = 3;
+
+pub struct Level {
+    level: u8,
+}
+
+impl Level {
+    pub fn new() -> Self {
+        Self { level: 1 }
+    }
+
+
+
+
+
+}
