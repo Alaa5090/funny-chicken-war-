c9bb2bc016a14e61c492c2404aa2cc7d4aaec970
Alaa5090 <alaaabdraboo297@gmail.com>
2023-12-12 11:40:50 +0200

    create kill_player_at function

--- src/player.rs
+++ src/player.rs
@@ -34,6 +34,18 @@ pub fn move_right(&mut self) {
         self.x += 1;
     }
 }
+pub fn kill_player_at(&mut self, x: usize, y: usize) -> bool {
+    if  self.x==x && self.y==y//position(|player| (player.x == x) && (player.y == y))
+    {
+        self.killed=true;
+        self.x=NUM_COLS / 2;
+        self.y=NUM_ROWS - 1;
+        true
+    } 
+    else {
+        false
+    }
+}
 
 
 
